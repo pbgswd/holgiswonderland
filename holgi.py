384d4846c8eb6bdb0cd446e487ad00ec6e279a12
Peter Gordon Linux Desktop <superwebdeveloper@gmail.com>
2017-04-14 12:13:11 -0700

    cleared out comments

--- holgi.py
+++ holgi.py
@@ -2,30 +2,30 @@ from moviepy.editor import *
 
 audioclip = AudioFileClip("bensound-funnysong.mp3").subclip(0, 144)
 
-intro = VideoFileClip('DSC_0022.AVI').subclip(5, 8) #3
-clip1 = VideoFileClip('DSC_0001.AVI').subclip(3,15) #12
-clip2 = VideoFileClip('DSC_0001.AVI').subclip(38,55) #17
-clip3 = VideoFileClip('DSC_0003.AVI').subclip(4,8) #4
-clip4 = VideoFileClip('DSC_0004.AVI').subclip(3,16) #13
-clip5 = VideoFileClip('DSC_0005.AVI').subclip(3,16) #13
-clip6 = VideoFileClip('DSC_0006.AVI').subclip(3,7) #4
-clip71 = VideoFileClip('DSC_0007.AVI').subclip(2,9) #7
-clip72 = VideoFileClip('DSC_0007.AVI').subclip(16,32) #16
-clip8  = VideoFileClip('DSC_0008.AVI').subclip(11,16) #5
-clip9  = VideoFileClip('DSC_0009.AVI').subclip(4,10) #6
-clip10 = VideoFileClip('DSC_0010.AVI').subclip(4,7) # 3
-clip11 = VideoFileClip('DSC_0011.AVI').subclip(9,15) # 6
-clip12 = VideoFileClip('DSC_0012.AVI').subclip(4,13)# 9
-clip13 = VideoFileClip('DSC_0013.AVI').subclip(5,10) #5
-clip14 = VideoFileClip('DSC_0014.AVI').subclip(2,7) #5
-clip15 = VideoFileClip('DSC_0015.AVI').subclip(62,72) # 10
-clip16 = VideoFileClip('DSC_0016.AVI').subclip(8,13) # 5
-clip17 = VideoFileClip('DSC_0017.AVI').subclip(14,24) #10
-clip18 = VideoFileClip('DSC_0018.AVI').subclip(18,23) #5
-clip19 = VideoFileClip('DSC_0019.AVI').subclip(8,20) # 12
-clip20 = VideoFileClip('DSC_0020.AVI').subclip(11,14) # 3
-clip21 = VideoFileClip('DSC_0021.AVI').subclip(31,44) # 13
-#171 seconds
+intro = VideoFileClip('DSC_0022.AVI').subclip(5, 8) 
+clip1 = VideoFileClip('DSC_0001.AVI').subclip(3,15) 
+clip2 = VideoFileClip('DSC_0001.AVI').subclip(38,55)
+clip3 = VideoFileClip('DSC_0003.AVI').subclip(4,8)
+clip4 = VideoFileClip('DSC_0004.AVI').subclip(3,16)
+clip5 = VideoFileClip('DSC_0005.AVI').subclip(3,16)
+clip6 = VideoFileClip('DSC_0006.AVI').subclip(3,7)
+clip71 = VideoFileClip('DSC_0007.AVI').subclip(2,9)
+clip72 = VideoFileClip('DSC_0007.AVI').subclip(16,32)
+clip8  = VideoFileClip('DSC_0008.AVI').subclip(11,16)
+clip9  = VideoFileClip('DSC_0009.AVI').subclip(4,10)
+clip10 = VideoFileClip('DSC_0010.AVI').subclip(4,7)
+clip11 = VideoFileClip('DSC_0011.AVI').subclip(9,15)
+clip12 = VideoFileClip('DSC_0012.AVI').subclip(4,13)
+clip13 = VideoFileClip('DSC_0013.AVI').subclip(5,10)
+clip14 = VideoFileClip('DSC_0014.AVI').subclip(2,7)
+clip15 = VideoFileClip('DSC_0015.AVI').subclip(62,72)
+clip16 = VideoFileClip('DSC_0016.AVI').subclip(8,13)
+clip17 = VideoFileClip('DSC_0017.AVI').subclip(14,24)
+clip18 = VideoFileClip('DSC_0018.AVI').subclip(18,23)
+clip19 = VideoFileClip('DSC_0019.AVI').subclip(8,20)
+clip20 = VideoFileClip('DSC_0020.AVI').subclip(11,14)
+clip21 = VideoFileClip('DSC_0021.AVI').subclip(31,44)
+
 content = concatenate_videoclips([intro.fadein(1).crossfadeout(1),
                                   clip1.crossfadein(1),
                                   clip2.crossfadein(0.5),
@@ -64,9 +64,9 @@ credits = concatenate_videoclips([vcredit,
 
 end = concatenate_videoclips([intro, intro]) 
 
-allcredits = CompositeVideoClip([end, credits.set_pos(('center','bottom'))]) #6
+allcredits = CompositeVideoClip([end, credits.set_pos(('center','bottom'))]) 
 
-video      = concatenate_videoclips([content, allcredits]) #171 + 6 seconds = 177 seconds
+video      = concatenate_videoclips([content, allcredits])
 final      = video.set_audio(audioclip)
 
 final.write_videofile("holgis_wonderland.avi",fps=24, codec='libx264')
