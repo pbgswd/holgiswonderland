0ba52a2c952a7157d406a43c077c94a358aa4f01
peter gordon macbook pro <superwebdeveloper@gmail.com>
2017-04-13 21:01:39 -0700

    checking in mp3, changes to py file

--- holgi.py
+++ holgi.py
@@ -1,20 +1,56 @@
 from moviepy.editor import *
+
+audioclip = AudioFileClip("bensound-funnysong.mp3").subclip(0, 152)
+
 intro = VideoFileClip('DSC_0022.AVI').subclip(5, 8)
 clip1 = VideoFileClip('DSC_0001.AVI').subclip(3,15)
 clip2 = VideoFileClip('DSC_0001.AVI').subclip(38,55)
 clip3 = VideoFileClip('DSC_0003.AVI').subclip(4,8)
 clip4 = VideoFileClip('DSC_0004.AVI').subclip(3,16)
 clip5 = VideoFileClip('DSC_0005.AVI').subclip(3,16)
+clip6 = VideoFileClip('DSC_0006.AVI').subclip(3,7)
+clip7 = VideoFileClip('DSC_0007.AVI').subclip(2,25)
+clip8 = VideoFileClip('DSC_0008.AVI').subclip(11,16)
+clip9 = VideoFileClip('DSC_0009.AVI').subclip(4,10)
+clip10 = VideoFileClip('DSC_0010.AVI').subclip(4,7)
+clip11 = VideoFileClip('DSC_0011.AVI').subclip(9,15)
+clip12 = VideoFileClip('DSC_0012.AVI').subclip(4,13)
+clip13 = VideoFileClip('DSC_0013.AVI').subclip(5,10)
+clip14 = VideoFileClip('DSC_0014.AVI').subclip(2,7)
+clip15 = VideoFileClip('DSC_0015.AVI').subclip(62,72)
+clip16 = VideoFileClip('DSC_0016.AVI').subclip(8,13)
+clip17 = VideoFileClip('DSC_0017.AVI').subclip(14,24)
+clip18 = VideoFileClip('DSC_0018.AVI').subclip(18,23)
+clip19 = VideoFileClip('DSC_0019.AVI').subclip(8,20)
+clip20 = VideoFileClip('DSC_0020.AVI').subclip(11,14)
+clip21 = VideoFileClip('DSC_0021.AVI').subclip(31,44)
 
- 
+
 #DSC_0005.AVI DSC_0006.AVI DSC_0007.AVI DSC_0008.AVI DSC_0009.AVI DSC_0010.AVI DSC_0011.AVI DSC_0012.AVI
 #DSC_0013.AVI DSC_0014.AVI DSC_0015.AVI DSC_0016.AVI DSC_0017.AVI DSC_0018.AVI DSC_0019.AVI  DSC_0020.AVI  DSC_0021.AVI
 
-content = concatenate_videoclips([intro.crossfadein(1).crossfadeout(1),
-                                  clip1.crossfadein(1).crossfadeout(0.5),
-                                  clip2.crossfadein(0.5).crossfadeout(0.5),
-                                  clip3.crossfadein(0.5).crossfadeout(0.5),
-                                  clip4.crossfadein(0.5).crossfadeout(0.5)
-                                  ])
- 
-content.write_videofile("holgis_wonderland.avi",fps=24, codec='libx264')
+content = concatenate_videoclips([intro.fadein(1).crossfadeout(1),
+                                  clip1.crossfadein(1),
+                                  clip2.crossfadein(0.5),
+                                  clip3.crossfadein(0.5),
+                                  clip4.crossfadein(0.5),
+                                  clip5.crossfadein(0.5),
+                                  clip6.crossfadein(0.5),
+                                  clip7.crossfadein(0.5),
+                                  clip8.crossfadein(0.5),
+                                  clip9.crossfadein(0.5),
+                                  clip10.crossfadein(0.5),
+                                  clip11.crossfadein(0.5),
+                                  clip12.crossfadein(0.5),
+                                  clip13.crossfadein(0.5),
+                                  clip14.crossfadein(0.5),
+                                  clip15.crossfadein(0.5),
+                                  clip16.crossfadein(0.5),
+                                  clip17.crossfadein(0.5),
+                                  clip18.crossfadein(0.5),
+                                  intro.crossfadein(0.5).fadeout(1)],
+                                  padding=-1, method="compose"                                  
+                                  )
+final = content.set_audio(audioclip)
+#Music: http://www.bensound.com
+final.write_videofile("holgis_wonderland.avi",fps=24, codec='libx264')
